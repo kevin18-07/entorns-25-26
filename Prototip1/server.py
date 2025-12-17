b2263615f7d6a4dd4bddbc3bd8a94a2f57ee3a36
kevin18-07 <cabrerahuacso@gmail.com>
2025-12-17 13:32:21 +0100

    commit

--- Prototip1/server.py
+++ Prototip1/server.py
@@ -1,5 +1,44 @@
 from flask import Flask, jsonify, request
 
+class User:
+    def __init__(self,username, nom, password, email, rol="tutor"):
+        self.username=username
+        self.nom=nom
+        self.password=password
+        self.email=email
+        self.rol=rol
+    
+    def __str__(self):
+        return self.nom
+
+#us1=User(username="ama",nom="Rob Halford",password="12345", email="rob@gmail.com",rol="tutor")
+#print(us1)
+users = [
+    User(username="rob",nom="Rob Halford",password="12345", email="rob@gmail.com",rol="tutor"),
+    User(username="john",nom="John Cannigan",password="12345", email="john@gmail.com",rol="tutor"),
+    User(username="maria",nom="Maria Sams",password="12345", email="maria@gmail.com",rol="admin")
+]
+
+class UserDao:
+    def __init__(self):
+        self.users=users
+    
+    def getUserByUsername(self,uname):
+        user = None
+        for u in self.users:
+            if u.username == uname:
+                user = u.__dict__
+        return user
+
+# Test DAO
+user_dao = UserDao()
+response=user_dao.getUserByUsername("pedro")
+print(response)
+response=user_dao.getUserByUsername("JJJJ")
+print(response)
+# End TEST
+
+'''
 app = Flask(__name__)
 
 @app.route('/user',methods=['GET'])
@@ -21,3 +60,4 @@ def user():
 
 if __name__ == '__main__':
     app.run(debug=True)
+'''
