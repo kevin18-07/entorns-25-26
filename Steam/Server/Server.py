dd761603742a6326bdb075f08df7dbf6791712c8
kevin18-07 <cabrerahuacso@gmail.com>
2026-05-20 13:32:12 +0200

    c

--- Steam/Server/Server.py
+++ Steam/Server/Server.py
@@ -122,4 +122,4 @@ def add_library():
 # ---------------- RUN ----------------
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10030, debug=True)
